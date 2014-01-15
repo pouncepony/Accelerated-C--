0dc9530a14993442c9a9c2213f99b93330f34e71
pouncepony <neil.copland@gmail.com>
2014-01-15 19:52:01 +0000

    Initial Commit

---
+++ AcceleratedC/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  AcceleratedC
+//
+//  Created by Neil on 15/01/2014.
+//  Copyright (c) 2014 Neil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
